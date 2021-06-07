96ce22cdab4f17d6f0da403ec2fcf8848eaab607
NicolasPenon <nicolaspenon@outlook.dk>
2021-06-07 02:39:52 +0200

    Opdatering registrering af bruger
    virker ikke endnu

--- index.php
+++ index.php
@@ -1,63 +1,49 @@
 <?php
 include("functions.php");
+session_start();
 
-$username = $password = $confirm_password = "";
-$username_error = $password_error = $confirm_password_error = "";
+$username = "";
+$mail = "";
+$city = "";
+$birth_date = "";
+$errors = array();
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-  if(empty(trim($_POST['username']))) {
-    $username_error = "Venligst skriv et brugernavn.";
-  } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST['username']))){
-      $username_error = "Brugernavn kan kun indeholde bogstaver, tal og underscore.";
-  } else{
-      $sql = "SELECT id FROM users WHERE username = ?";
+if(isset($_POST['registration'])) {
+  $username = mysqli_real_escape_string($conn, $_POST['username']);
+  $mail = mysqli_real_escape_string($conn, $_POST['mail']);
+  $password = mysqli_real_escape_string($conn, $_POST['password']);
+  $confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
+  $city = mysqli_real_escape_string($conn, $_POST['city']);
+  $birth_date = mysqli_real_escape_string($conn, $_POST['birth_date']);
 
-      if($stmt = mysqli_prepare($conn, $sql)){
-        mysqli_stmt_bind_param($stmt, "s", $param_username);
-        $param_username = trim($_POST['username']);
-        if(mysqli_stmt_execute($stmt)){
-          mysqli_stmt_store_result($stmt);
-          if(mysqli_stmt_num_rows($stmt) == 1){
-            $username_error = "Dette brugernavn er allerede i brug.";
-          } else{
-            $username = trim($_POST['username']);
-          }
-        } else{
-          echo "Noget gik galt, prøv igen senere.";
-        }
-        mysqli_stmt_close($stmt);
-      }
+  if (empty($username)) { array_push($errors, "Brugernavn påkrævet"); }
+  if (empty($mail)) { array_push($errors, "E-mail påkrævet"); }
+  if (empty($password)) { array_push($errors, "Kodeord påkrævet"); }
+  if ($password != $confirm_password) {
+    array_push($errors, "Kodeord er ikke ens");
+  }
+
+  $username_check = "SELECT * FROM users WHERE username='$username' OR mail='$mail' LIMIT 1";
+  $results = mysqli_query($conn, $username_check);
+  $user = mysqli_fetch_assoc($results);
+
+  if($user) {
+    if($user['usernamer'] === $username) {
+      array_push($errors, "Brugernavn er allerede i brug");
     }
-    if(empty(trim($_POST['password']))){
-      $password_error = "Venligst indtast et kodeord.";
-    } elseif(strlen(trim($_POST['password'])) < 8){
-      $password_error = "Kodeord skal være længere end 8 tegn.";
-    } else{
-      $password = trim($_POST['password']);
+    if($user['mail'] === $mail) {
+      array_push($errors, "E-mail er allerede i brug");
     }
-    if(empty(trim($_POST['confirm_password']))){
-      $confirm_password_error = "Bekræft kodeord.";
-    } else{
-      $confirm_password = trim($_POST['confirm_password']);
-      if(empty($password_error) && ($password != $confirm_password)){
-        $confirm_password_error = "Kodeordene er ikke ens.";
-      }
-    }
-    if(empty($username_error) && empty($password_error) && empty($confirm_password_error)) {
-      $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-      if($stmt = mysqli_prepare($conn, $sql)) {
-        mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-        $param_username = $username;
-        $param_password = password_hash($password, PASSWORD_DEFAULT);
-        if(mysqli_stmt_execute($stmt)){
-          header("location: login.php");
-        } else{
-          echo "Prøv igen du.";
-        }
-        mysqli_stmt_close($stmt);
-      }
-    }
-    mysqli_close($conn);
+  }
+
+  if(count($errors) == 0) {
+    $password = md5($password);
+    $query = "INSERT INTO users (username, mail, password, city, birth_date) VALUES('$username','$mail', '$password','$city','$birth_date')";
+    mysqli_query($conn, $query);
+    $_SESSION['username'] = $username;
+    $_SESSION['success'] = "Du er nu logget ind";
+    header('location: index.php');
+  }
 }
 ?>
 <!DOCTYPE html>
@@ -67,15 +53,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <title></title>
   </head>
   <body>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+    <form action="index.php" method="post">
+      <?php include('errors.php'); ?>
       <label>Brugernavn</label>
       <input type="text" name="username" value="<?php echo $username; ?>">
+      <label>E-mail</label>
+      <input type="email" name="mail" value="<?php echo $email; ?>">
       <label>Kodeord</label>
       <input type="password" name="password" value="<?php echo $password; ?>">
       <label>Bekræft kodeord</label>
       <input type="password" name="confirm_password" value="<?php echo $confirm_password; ?>">
-      <input type="submit" value="Bekræft">
-
+      <label>By</label>
+      <input type="text" name="city" value="<?php echo $city; ?>">
+      <label>Fødselsdato</label>
+      <input type="date" name="birth_date" value="<?php echo $birth_date; ?>">
+      <button type="submit" name="registration">Bekræft</button>
     </form>
 
   </body>
