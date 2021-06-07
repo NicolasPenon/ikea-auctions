9a58df92c32da221683ddf54f84011f79779ceb4
NicolasPenon <nicolaspenon@outlook.dk>
2021-06-07 21:19:11 +0200

    opdateret søgningen

--- search_list.php
+++ search_list.php
@@ -7,17 +7,18 @@ $output='';
 if(isset($_POST['search'])) {
   $searchq = $_POST['search'];
 
-  $query = mysqli_query($conn, "SELECT * FROM auctions WHERE name LIKE '%$searchq%'");
+  $query = mysqli_query($conn, "SELECT * FROM auctionall WHERE name LIKE '%$searchq%'");
   $count = mysqli_num_rows($query);
   if($count==0) {
     $output='';
   } else{
     while($row=mysqli_fetch_array($query)) {
+      $id=$row['id'];
       $name=$row['name'];
       $category=$row['category'];
       $status=$row['status'];
 
-      $output .= '<div class="name_div"> '.$name.'</div><div class="category_div"> '.$category.'</div><div class="status_div"> '.$status.'</div>';
+      $output .= '<div class="id_div"> '.$id.'</div><div class="name_div"> '.$name.'</div><div class="category_div"> '.$category.'</div><div class="status_div"> '.$status.'</div>';
     }
   }
 }
