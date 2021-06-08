3e24cbbce255c0ca2564da7ad97025b43d8371eb
Markdalin <Mdpe27852@edu.ucl.dk>
2021-06-08 23:10:48 +0200

    css nav.php update

--- nav.php
+++ nav.php
@@ -12,19 +12,26 @@ $category = mysqli_query($conn, $query);
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="css/nav.css">
   </head>
   <body>
 
-<h1> Kategorier </h2>
+<div id="category">
+  <h1> Kategorier </h2>
+</div>
+<div id="allproducts">
     <div onclick="location.href='frontpage.php';" class="products_box">
       <a class="rtext">Alle produkter</a>
     </div>
+</div>
 
 <?php
     while($row=mysqli_fetch_assoc($category)) { ?>
+    <div id ="categorybox">
       <li>
         <a href="frontpage.php?catid=<?php echo $row['id']; ?>"><?php echo $row['category']; ?></a>
       </li>
+    </div>
   <?php } ?>
 
   </body>
